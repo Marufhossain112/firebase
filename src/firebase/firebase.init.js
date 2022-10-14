a9d2ef9644ae4fab478a4108c462f5cd75e9c931
maruf <dev.maruf112@gmail.com>
2022-10-14 13:58:09 +0600

    firebase authentication added

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMIYwvHxECkpmssYt9K_QPouMovBOtlCg",
+  authDomain: "reboot-masta.firebaseapp.com",
+  projectId: "reboot-masta",
+  storageBucket: "reboot-masta.appspot.com",
+  messagingSenderId: "493100287692",
+  appId: "1:493100287692:web:99baf7e066d28f21f4e469",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
